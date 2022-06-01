4b26166b44a799c991ce634acd8d1bcfcec30dfa
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-06-01 18:10:43 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,95 @@
-
+namespace dice {
+    /**
+     * make a image dice
+     */
+    //% block
+    export function makeImageDice(): void {
+         
+        basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    }
+    /**
+     * use images from image 1
+     */
+    //% block
+    export function image1Dice(): void {
+        basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    }
+    /**
+     *  Image 2
+     */
+    //% block
+    export function image2Dice(): void {
+        basic.showLeds(`
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . .
+        `)
+    }
+    /**
+     *  Image 3
+     */
+    //% block
+    export function image3Dice(): void {
+        basic.showLeds(`
+        . . . . #
+        . . . . .
+        . . # . .
+        . . . . .
+        # . . . .
+        `)
+    }
+    /**
+     *  Image 4
+     */
+    //% block
+    export function image4Dice(): void {
+        basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    }
+    /**
+     *  Image 5
+     */
+    //% block
+    export function image5Dice(): void {
+        basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . # . .
+        . . . . .
+        # . . . #
+        `)
+    }
+    /**
+     *  Last dice image
+     */
+    //% block
+    export function image6Dice(): void {
+        
+        basic.showLeds(`
+        # . . . #
+        . . . . .
+        # . . . #
+        . . . . .
+        # . . . #
+        `)
+    }
+}
